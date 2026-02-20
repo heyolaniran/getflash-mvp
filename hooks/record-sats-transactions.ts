23c5f80e02df67d0177027e02c543cf6c8426dbc
heyolaniran <olaniran.abd@gmail.com>
2026-02-20 10:01:54 +0100

    feat : sats balance

--- hooks/record-sats-transactions.ts
+++ hooks/record-sats-transactions.ts
@@ -15,3 +15,30 @@ export default async function recordSatsTransactions({ payload }: { payload: any
     return data;
 
 }
+
+export async function getSatsBalance() {
+    const supabase = createClient();
+
+    const { data: user, error: userError } = await supabase.auth.getUser();
+
+    if (userError) {
+        console.log("Error", userError);
+        return 0;
+    }
+
+    const { data, error } = await supabase.from("sats_transactions")
+        .select("amount,type")
+        .eq("user_id", user?.user?.id)
+        .eq("status", "success")
+        .in("type", ["received", "sent"]);
+    if (error) {
+        console.log("Error", error);
+        return 0;
+    }
+
+    return data.reduce((acc, curr) => {
+        if (curr.type === "received") return acc + curr.amount;
+        if (curr.type === "sent") return acc - curr.amount;
+        return acc;
+    }, 0);
+}
